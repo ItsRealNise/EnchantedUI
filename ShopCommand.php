8dcecef8714b3619ed9f45e6ada93021b4f5f2dc
ItsRealNise <90197402+ItsRealNise@users.noreply.github.com>
2022-01-31 05:13:06 +0700

    Add files via upload

---
+++ ShopCommand.php
@@ -0,0 +1,52 @@
+<?php
+namespace ItsRealNise\EnchantUI\Commands;
+
+use pocketmine\plugin\Plugin;
+use pocketmine\command\{
+    Command,
+    PluginCommand,
+    CommandSender
+};
+use pocketmine\plugin\PluginOwned;
+use pocketmine\Player;
+use ItsRealNise\EnchantUI\Main;
+
+/**
+ * Class ShopCommand
+ * @package ItsRealNise\EnchantUI\Commands
+ */
+class ShopCommand extends Command implements PluginOwned {
+    
+    /**
+    * ShopCommand constructor.
+    * @param Main $plugin
+    */
+    public function __construct(Main $plugin){
+        parent::__construct("eshop", "Enchant Shop commands", \null, ["eshop"]);
+        $this->plugin = $plugin;
+    }
+    
+   /**
+    * @param CommandSender $sender
+    * @param string $commandLabel
+    * @param array $args
+    *
+    * @return bool
+    */
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool{
+        if(!$sender->hasPermission("eshop.command")){
+            $sender->sendMessage($this->plugin->shop->getNested('messages.no-perm'));
+            return true;
+        }
+        /**if(!$sender instanceof Player){
+            $sender->sendMessage("Please use this in-game.");
+            return true;
+        }*/  
+        $this->plugin->listForm($sender);
+        return true;
+	}
+   
+   public function getOwningPlugin(): Plugin {
+		return $this->plugin;
+	}
+}
