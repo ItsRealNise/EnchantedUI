a9c00493193e2e3f8a45811e3915669a82a83e81
ItsRealNise <90197402+ItsRealNise@users.noreply.github.com>
2022-02-12 12:41:27 +0700

    Change

--- src/ItsRealNise/EnchantedUI/Main.php
+++ src/ItsRealNise/EnchantedUI/Main.php
@@ -48,10 +48,6 @@ class Main extends PluginBase{
         $this->getServer()->getCommandMap()->register("enchantui", new ShopCommand($this));
         $this->piggyCE = $this->getServer()->getPluginManager()->getPlugin("PiggyCustomEnchants");
         $this->eco = $this->getServer()->getPluginManager()->getPlugin("FormAPI");
-        if($this->eco == null) {
-           $this->getServer()->getPluginManager()->disablePlugins();
-           $this->getServer()->getLogger()->alert("EconomyAPI not found disable the plugins...");
-        }
     }
 
     public function UpdateConfig(): void{
